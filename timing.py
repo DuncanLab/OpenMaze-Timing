ee05dfd056a1cfa9667cc6f4e587b0d9201775b1
Alex Gordienko <alexgor99@gmail.com>
2018-10-23 18:52:24 -0400

    added comments for transparency to timing file

--- timing.py
+++ timing.py
@@ -1,27 +1,41 @@
 import RPi.GPIO as GPIO
 import time
-import threading
+import threading # for threading to ensure time consistency
 import datetime
 
+# Configure sensor input from Raspberry pi
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.IN)
-GPIO.setup(18, GPIO.IN)
+GPIO.setup(4, GPIO.IN) # photo sensor IN at pin 4
+GPIO.setup(18, GPIO.IN) # sound sensor IN at pin 18
 
 class Timing:
+    '''
+    Overarching class containing all timing-relevant functions.
+
+    The purpose of this class is to facillatate accurate timing
+    of certain events for OpenMaze (ie when a new trial starts
+    and a new screen appears in front of the user, is unity
+    reporting the same information as what is actually going on?)
+
+    Two basic sensors are used: a photo light sensor and a sound
+    sensor are leveraged to trigger timing events between the
+    experiment running in Unity and the raspberry pi. 
+    
+    '''
     def __init__(self, startTime):
         '''
-        Initialize class current time.
+        Initialize class with current time and store original
+        time.
         '''
         self.startTime = startTime
-        print(self.startTime) # print class start time.
+        print(self.startTime) # print class start time for reference
 
     def printSensorStream(self):
         '''
-        Basic function to print out function start time
-        and print a stream of sensor data as a tuple. 
+        Basic function to print out a stream of sensor data to the
+        console. 
         '''
-        print(self.startTime)
         while True:
             # flip photo sensore value for consistency with sounds sensor 
             print(int(not GPIO.input(4)), GPIO.input(18))
@@ -29,18 +43,16 @@ class Timing:
     def sensorDataAndTime(self):
         '''
         Function prints out sensor data as
-        photo / sound / time in milliseconds since
-        start time. 
+        (photo / sound / time in milliseconds since
+        start time). 
         '''
-        
+        # open a new thread and report sensor data every x seconds
         threading.Timer(5.00000, self.sensorDataAndTime).start()
         print(str(int(not GPIO.input(4))) +
               '/' + str(GPIO.input(18)) +
               '/' + str(datetime.datetime.now()-self.startTime))
         
-        
-timingTrial = Timing(datetime.datetime.now())
-timingTrial.sensorDataAndTime();
-        
 
-
+timingTrial = Timing(datetime.datetime.now()) #initialize class with current time
+timingTrial.sensorDataAndTime(); # run function of choice
+        
